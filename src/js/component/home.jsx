9d36162665b4c5fb7d37fc7e6e99f438a5f8aaad
Jose Pindado <joseangelpindado@gmail.com>
2024-11-08 19:45:00 +0000

    First commit

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,40 +1,95 @@
-import React, { useState } from "react";
-import TodoList from "./todolist";
+import React, { useEffect, useState } from "react";
+
+import Carta from "./Carta";
+import Cabecera from "./Cabecera";
 
 
+//create your first component
 const Home = () => {
-  const [tasks, setTasks] = useState([]);
-  const [inputValue, setInputValue] = useState("");
+  //Hacemos que la api cree un usuario nada más carge la web
+  useEffect(() => {
+    fetch("https://playground.4geeks.com/todo/users/josepindado", {
+      headers: {
+        accept: "application/json",
+        "Content-type": "application/json",
+      },
+      method: "POST"
+    })
+  }, []);
+  const datos = () =>{
+    fetch("https://playground.4geeks.com/todo/users/josepindado")
+      .then(response => response.json())
+      .then(response => setTask(response.todos))
+  } 
+  useEffect(()=>{
+    datos()
+  }, []);
 
-  const handleAddTask = (e) => {
-    if (e.key === "Enter" && inputValue.trim()) {
-      setTasks([...tasks, inputValue.trim()]);
-      setInputValue("");
+  //Creamos una variable vacía para que almacene los datos del input
+  const [tarea, setTarea] = useState("");
+
+  // Creamos un array vacio para que almacene los datos que le llegan de la variable de tarea
+  const [task, setTask] = useState([{}]);
+
+  //Creamos la funcion para que el programa lea el valor que hay en el input y lo almacene en tarea
+  const handleChange = (event) => {
+    setTarea(event.target.value);
+  };
+
+
+  //Creamos la funcion para que añada un elemento nuevo al array task
+  const nuevaTarea = () => {
+    //Validamos que si no hay informacion en la variable tarea no pueda añadir nada al array de task
+    if (tarea == "") {
+      alert("Debes crear una tarea");
+    } else {
+      //Creamos un objeto para almacenar la tarea
+      const newTask = {
+        label: tarea,
+        is_done: false
+      }
+      //hacemos el fetch para que cree la tarea
+      fetch("https://playground.4geeks.com/todo/todos/josepindado", {
+        method: "POST",
+        headers: {
+          accept: "application/json",
+          "Content-type": "application/json",
+        },
+        body: JSON.stringify(newTask)
+      });
+      datos();
+      setTarea();
     }
   };
 
-  const handleDeleteTask = (index) => {
-    const updatedTasks = tasks.filter((_, i) => i !== index);
-    setTasks(updatedTasks);
-  };
 
   return (
-    <div className="container mt-5">
-      <h1 className="text-center">TO-DO List</h1>
-      <div className="input-group mb-3">
-        <input
-          type="text"
-          className="form-control"
-          placeholder="Añadir una tarea..."
-          value={inputValue}
-          onChange={(event) => setInputValue(event.target.value)}
-          onKeyDown={handleAddTask}
-        />
+    <div>
+      <Cabecera input={handleChange} nuevaTarea={nuevaTarea} />
+      <div className="row tareas">
+        {task.length > 0 ? (
+          //Mapeamos el array que tenemos en cada momento
+          task.map((element) => {
+            //Creamos la funcion que elimina la carta seleccionada
+            const eliminarTarea = () => {
+              fetch(`https://playground.4geeks.com/todo/todos/${element.id}`, {
+                method: "DELETE"
+              })
+              datos();
+            };
+            //Retornamos una carta por cada elemento del array
+            return (
+              <>
+                <Carta task={element.label} eliminarTarea={eliminarTarea} />
+              </>
+            );
+          })
+        ) : (
+          <div className="tareasPendientes">
+            <p>No hay tareas, añadir tareas</p>
+          </div>
+        )}
       </div>
-      <TodoList tasks={tasks} handleDeleteTask={handleDeleteTask} />
-      {tasks.length === 0 && (
-        <p className="text-center text-muted">No hay tareas, añadir tareas.</p>
-      )}
     </div>
   );
 };
